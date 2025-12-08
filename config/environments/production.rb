baff9d12976647dfbc3bf17808d651037e024749
James Guest <jguest@endpoints.news>
2025-12-08 12:58:35 +0000

    feat: prod values

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,15 @@ Rails.application.configure do
   #   enable_starttls_auto: true
   # }
 
+  config.action_mailer.smtp_settings = {
+    address: "email-smtp.us-east-1.amazonaws.com",
+    port: 587,
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
